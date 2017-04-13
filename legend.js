92305bf8df9cd2444b14fac1e6839e3cca5052d6
Oliver Isenrich <IsenrichO@gmail.com>
2017-04-13 01:44:47 -0700

    docs (Legend): Include context for the application

---
+++ legend.js
@@ -0,0 +1,18 @@
+
+/*
+ * Constant are most recognizable in that they are referred to using ALL-CAPS, in
+ *  line with what JavaScript best practices dictates for true constants. Certain
+ *  caveats bear mention:
+ *
+ *  > Some constants may be used in any of a variety of different but (within loosely
+ *    defined bounds of precision) are collectively equivalent. These varietals may
+ *    be recognized based on the presence of a here-defined standardized suffix.
+ *    1.) The `_EXP` suffix is used to convey that the referred-to constant is given
+ *        in its EXPanded for (usually up to 35 decimal places) and is most appropriate
+ *        where non-arbitrary, higher degrees of precision are required (e.g., in
+ *        contexts of mathematical analysis).
+ *    2.) The `_ID` suffix is used to convey that the referred-to constant is given
+ *        in the form of an IDentity expression; that is to say, as an exact equivalent
+ *        whose value is held as an expression rather than a non-terminating or repeating
+ *        (and, therefore, non-exact) decimal approximation.
+ */
