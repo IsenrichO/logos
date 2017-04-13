acd57cc10d5506ea517826ae8b14cb300ff0e008
Oliver Isenrich <IsenrichO@gmail.com>
2017-04-13 02:29:36 -0700

    feat (Algebra): Further abstract the reduce() function

--- algebra/index.js
+++ algebra/index.js
@@ -1,15 +1,29 @@
 
 // Mapping of mathematical operators to their corresponding functional representation:
-const MathOps = new Map([
-  ['+', (val1, val2) => val1 + val2],
-  ['-', (val1, val2) => val1 - val2],
-  ['*', (val1, val2) => val1 * val2],
-  ['/', (val1, val2) => val1 / val2],
+export const MathOps = new Map([
+  ['+', (val1, val2) => val1 + val2], // Addition
+  ['-', (val1, val2) => val1 - val2], // Subtraction
+  ['*', (val1, val2) => val1 * val2], // Multiplication
+  ['/', (val1, val2) => val1 / val2], // Division
 
-  ['^', (val1, val2) => Math.pow(val1, val2)],
-  ['**', (val1, val2) => Math.pow(val1, val2)]
+  ['^', (val1, val2) => Math.pow(val1, val2)],  // "<VAL1> To the power of <VAL2>"
+  ['**', (val1, val2) => Math.pow(val1, val2)]  // Exponentiation
 ]);
 
 
 // Returns the reciprocal of a number or fraction:
-const reciprocal = (num = 1) => (1 / num);
+export const reciprocal = (num = 1) => (1 / num);
+
+// Returns the absolute value of the evaluated result of a contained input expression:
+export const abs = (exp) => Math.abs(exp);
+
+// Returns the arithmetic sum of a collection of values:
+export const sum = (vals = []) => vals.reduce((memo, curr) => memo += Number(curr), 0);
+
+// A variation of the native `.reduce()` method on the Array object, the `reducer()`
+//  function is intended to further abstract the repetitive nature of scripting out
+//  custom aggregation function by simply providing a list of values, the transform
+//  function against which to effect said aggregation and, optionally, an initial
+//  value <init>:
+export const reducer = (vals = [], transform = (a, b) => a + b, init) =>
+  vals.reduce((memo, curr) => transform(memo, curr), init);
