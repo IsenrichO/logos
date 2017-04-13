b095f61919299f4107bb25d786dcaa2954ef40a6
Oliver Isenrich <IsenrichO@gmail.com>
2017-04-13 01:44:25 -0700

    init (Constants): Setup reference files for mathematical constants Add most-used mathematical constants to index.js in the 'general' sub-folder Create Fahrenheit-to-Celsius function converter and vice-versa in 'chemistry' sub-folder

---
+++ constants/chemistry/index.js
@@ -0,0 +1,6 @@
+
+// Conversion from degrees Fahrenheit (°F) to degrees Celsius (°C):
+const FahrToCelsius = (degF) => (degF - 32) * (5 / 9);
+
+// Conversion from degrees Celsius (°C) to degrees Fahrenheit (°F):
+const CelsiusToFahr = (degC) => (degC * (9 / 5)) + 32;
---
+++ constants/general/index.js
@@ -0,0 +1,58 @@
+
+// Zero, 0, the basis upon which the number field of integers, ℤ, can be divided
+//  into those with positive (+) sign and those with negative (-) sign. It fulfills
+//  a central mathematical role for its allowance of the additive identity (a + 0 = a):
+const ZERO = 0;
+
+// (Positive) one, +1, (also known as unity or the unit value) and the fundamental
+//  unit of the field of natural numbers, ℕ, and another core player in the theoretic
+//   underpinnings of mathematics vís-a-vís the multiplicative identity (a * 1 = a):
+const ONE = 1;
+
+// (Negative) one, -1, the first item of the (ordered) field of negative integers,
+//  ℤ-, which can be dually seen as the additive inverse of 1 as well as the unique
+//  value from which all other negative integers are simple multiples thereof:
+const NEG_ONE = -1;
+
+// The Archimedean ratio, π, (also known as Ludolph's number) given to be the
+//  ratio of the circumference of a circle to the diamater of the same (C/d):
+const PI = 3.14,                                      // 2 dp
+      PI_EXP = 3.14159265358979323846264338327950288; // 35 dp
+
+// The natural logarithm base `e`, otherwise known as Euler's number and/or
+//  Napier's constant:
+const EULER_NUM = 2.718,                                      // 3 dp
+      EULER_NUM_EXP = 2.71828182845904523536028747135266249;  // 35 dp
+
+// The Euler-Mascheroni constant, γ, also known as Euler's constant (though not to be
+//  confused with the aforementioned Euler's number), is a recurring constant
+//  pervasive in the fields of analysis and number theory:
+const EULER_MASCHERONI = 0.577,                                     // 3 dp
+      EULER_MASCHERONI_EXP = 0.57721566490153286060651209008240243; // 35 dp
+
+// The imaginary unit, `i`, or unit imaginary value:
+const IMAGINARY_UNIT = Math.sqrt(-1);
+
+// Pythagoras' constant, given as the principal square root of two and that, when
+//  multiplied by itself, returns the integer value 2:
+const SQRT_2 = 1.414,                                     // 3 dp
+      SQRT_2_ID = Math.sqrt(2),                           // Identity expression
+      SQRT_2_EXP = 1.41421356237309504880168872420969807; // 35 dp
+
+// Theodorus' constant, given as the principal square root of three and that, when
+//  multiplied by itself, returns the integer value 3:
+const SQRT_3 = 1.732,                                       // 3 dp
+      SQRT_3_ID = Math.sqrt(3),                             // Identity expression
+      SQRT_3_EXP = 1.73205080756887729352744634150587236;   // 35 dp
+
+// The principal square root of five, an irrational algebraic constant whose use
+//  is profound within contexts surrounding the Golden Ratio:
+const SQRT_5 = 2.236,                                     // 3 dp
+      SQRT_5_ID = Math.sqrt(5),                           // Identity expression
+      SQRT_5_EXP = 2.23606797749978969640917366873127623; // 35 dp
+
+// The Golden Ratio, φ, a recurring and significant constant with algebraic as well
+//  as geometric interpretations. Its occurrence is especially profound for the
+//  domain of sacred geometry:
+const GOLDEN_RATIO = 1.618,                                       // 3 dp
+      GOLDEN_RATIO_EXP = 1.61803398874989484820458683436563811;   // 35 dp
