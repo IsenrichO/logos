ceb9706e4f30bcf8bc02255986754dd34aa16720
Oliver Isenrich <IsenrichO@gmail.com>
2017-04-13 02:29:03 -0700

    docs (Legend): Add explanation for included inline comments

--- legend.js
+++ legend.js
@@ -15,4 +15,15 @@
  *        in the form of an IDentity expression; that is to say, as an exact equivalent
  *        whose value is held as an expression rather than a non-terminating or repeating
  *        (and, therefore, non-exact) decimal approximation.
+ *    3.) The `_JS` suffix is used to convey that the referred-to constant is given
+ *        as the equivalently, native JavaScript representation of the same contained
+ *        as a static property of the `Math` object.
+ *
+ *  > Inline comments are included following many constants and are intended to provide
+ *    greater context into what a particular constant (or a particular representation
+ *    of some constant) is intended to do. In particular, those constants whose values
+ *    are held as truncated floats (i.e., a decimal approximation) will often be
+ *    followed with a short comment that includes some number followed by the `d.p.`
+ *    abbreviation [<NUMBER> d.p.]. This signifies that said constant is given to
+ *    NUMBER Decimal Places.
  */
